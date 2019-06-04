93451a300c643de0dcd27238a710c26f6bd18a64
mohammad <khezri@outlook.com>
2019-06-04 15:57:18 -0500

    finished setup

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App text-center'>
+        <h1 className="m-5 bg-dark p-4">Setup React</h1>
+
+      </div>
+    );
+  }
+}
+
+export default App;
+
