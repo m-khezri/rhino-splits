d409b1359022ac09afbe5ecb66f936498f37ce51
mohammad <khezri@outlook.com>
2019-06-11 19:21:09 -0500

    moved login button in navbar

--- src/components/MyNavar/MyNavbar.js
+++ src/components/MyNavar/MyNavbar.js
@@ -1,38 +1,49 @@
 import React from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-} from 'reactstrap';
 import './MyNavbar.scss';
+import authRequests from '../../helpers/data/authRequests';
+
 
 class MyNavbar extends React.Component {
   state = {
     isOpen: false,
   };
 
+  authenticateUser = (e) => {
+    e.preventDefault();
+    authRequests.authenticate().then(() => {
+    }).catch(err => console.error('there was an error with auth', err));
+  }
+
+
   render() {
     const { isAuthed, logoutClickEvent } = this.props;
     return (
-      <div className="my-navbar">
-        <Navbar color="dark" dark expand="md">
-          <NavbarBrand href="/">Rhino Splits</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                {isAuthed ? <NavLink onClick={logoutClickEvent}>Logout</NavLink> : ''}
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
+      <div>
+        <nav className="navbar navbar-expand-lg">
+          <a className="navbar-brand" href="#">Rhino Splits</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+
+              <li className="nav-item">
+                {isAuthed ?
+                  <a className='nav-link' href='#' onClick={logoutClickEvent}>Logout</a>
+                  :
+                  <li>
+                    <button className="btn btn-light" onClick={this.authenticateUser}>Login</button>
+                  </li>
+                }
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
 }
 
 export default MyNavbar;
+
