03b98c483613dd8f936c3aecbaceb81e8ea3fe88
mohammad <khezri@outlook.com>
2019-06-12 16:46:21 -0500

    read contacts but only the first two items!

--- src/components/ContactItem/ContactItem.js
+++ src/components/ContactItem/ContactItem.js
@@ -10,11 +10,13 @@ class ContactItem extends React.Component {
   render() {
     const { friend } = this.props;
     return (
-      <div>
-        <p>{friend.name}</p>
-        <p>{friend.lastname}</p>
-        <p>{friend.phone}</p>
-        <p>{friend.email}</p>
+      <div className='bg-light shadow-sm rounded w-100 m-2 p-2'>
+        <div className='d-flex flex-wrap'>
+          <p className='my-auto mr-4'><b>Name:</b> {friend.name}</p>
+          <p className='my-auto mr-4'><b>Last name:</b> {friend.lastname}</p>
+          <p className='my-auto mr-4'><b>Phone:</b> {friend.phone}</p>
+          <p className='my-auto mr-4'><b>E-mail:</b> {friend.email}</p>
+        </div>
       </div>
     );
   }
