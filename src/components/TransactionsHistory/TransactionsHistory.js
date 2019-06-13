65559800fd2f2a509bae345af13449c804582241
mohammad <khezri@outlook.com>
2019-06-12 22:44:50 -0500

    read transactions done

--- src/components/TransactionsHistory/TransactionsHistory.js
+++ src/components/TransactionsHistory/TransactionsHistory.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './TransactionsHistory.scss';
 import transactionsShape from '../../helpers/propz/transactionsShape';
+import TransactionsItem from '../TransactionsItem/TransactionsItem';
 
 class TransactionHistroy extends React.Component {
 
   static propTypes = {
-    transactions: Proptypes.arrayOf(transactionsShape),
+    transactions: PropTypes.arrayOf(transactionsShape),
   }
 
   render() {
+
+    const { transactions } = this.props;
+    const transactionItemComponents = transactions.map(transaction => (
+      <TransactionsItem
+        transaction={transaction}
+        key={transaction.id}
+      />
+    ));
+
     return (
       <div className="transaction-container m0 p-1">
         <p className="text-info">Transaction History</p>
-
+        <h2>{transactionItemComponents}</h2>
 
       </div>
     );
