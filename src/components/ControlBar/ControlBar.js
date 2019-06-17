a742ccedd6443f97a55f8017f12afea3f28ae930
mohammad <khezri@outlook.com>
2019-06-17 16:38:59 -0500

    new contact modal created

--- src/components/ControlBar/ControlBar.js
+++ src/components/ControlBar/ControlBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './ControlBar.scss';
+import NewContactForm from '../NewContactForm/NewContactForm';
 
 class ControlBar extends React.Component {
 
   render() {
     return (
-      <div className="control-bar-container m-0 p-4">
-        <button className="btn btn-danger btn-lg">Create contact</button>
+      <div className="control-bar-container m-0">
+        <NewContactForm />
       </div>
     );
   }
